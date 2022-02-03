e1636161589520d75dd15d396f5e34c1f451af80
Dimuthu Lakshan <w.d.lakshan10@gmail.com>
2022-02-03 16:48:44 +0530

    get the entered image to the flask

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def helloWorld():
-    return "hello World"
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def prediction():
+    predict_img = request.files['predict_image']
+    save_path = "./images/"+ predict_img.filename
+    predict_img.save(save_path)
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
